d85b7ef3f63258ad9ffba96d5d4a11fe420ea489
malelang <landiezm@unicauca.edu.co>
2019-01-30 09:37:01 -0500

    cambios medio hechos

---
+++ principal.js
@@ -0,0 +1,13 @@
+$("#btnlogin").hover(function(){$(this).toggleClass('login_hover');});
+
+function DoSomething(tipologin){
+    switch(tipologin){
+        case 1:
+            
+        case 2:
+        case 3:
+        case 4:
+
+    }
+
+}
