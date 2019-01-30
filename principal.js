5099af9c52ff23d392ea0ab187131e540f501d03
malelang <landiezm@unicauca.edu.co>
2019-01-30 10:12:43 -0500

    ya funciona jquery

--- principal.js
+++ principal.js
@@ -1,9 +1,13 @@
+$(document).ready(function() {
+    
+  });
+
 $("#btnlogin").hover(function(){$(this).toggleClass('login_hover');});
 
 function DoSomething(tipologin){
     switch(tipologin){
         case 1:
-            
+
         case 2:
         case 3:
         case 4:
